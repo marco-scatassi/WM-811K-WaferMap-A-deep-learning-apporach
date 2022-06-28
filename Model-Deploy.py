ef599a2015cef6616e9a171904a008b04ff28c7b
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-06-28 22:45:36 +0200

    Update Model-Deploy.py

--- Model-Deploy.py
+++ Model-Deploy.py
@@ -1,6 +1,11 @@
 import streamlit as st
+from PIL import Image
+
 
 st.set_page_config(layout = "wide")
 
-st.title("WM-811K WaferMap")
-st.write("Model evaluation for semiconductor wafermap detection and recognition using CNNs")
+if __name__ == '__main__':
+  st.title("WM-811K WaferMap")
+  st.write("Model evaluation for semiconductor wafermap detection and recognition using CNNs.")
+
+
