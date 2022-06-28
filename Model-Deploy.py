ec2aff079c2fdb91f38cac334fe226ee6ce78a3b
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-06-28 22:37:58 +0200

    Update Model-Deploy.py

--- Model-Deploy.py
+++ Model-Deploy.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.set_page_config(layout = "wide")
 
 st.title("WM-811K WaferMap")
+st.write("Model evaluation for semiconductor wafermap detection and recognition using CNNs")
