7159b2ad289cd02d44962354619f073fb8792484
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-07-06 12:48:09 +0200

    Update Model-Deploy.py

--- Model-Deploy.py
+++ Model-Deploy.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
   
   for i in range(10):
    image = Image.open(center_path+str(i)+'.png')
-   image = image.resize((600, 400))
+   image = image.resize((200, 200))
    st.image(image)
 
 
