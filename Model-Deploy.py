1adf55f9ae6289aba6206ca71530fed9114eaeb0
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-08-02 11:52:08 +0200

    Update Model-Deploy.py

--- Model-Deploy.py
+++ Model-Deploy.py
@@ -56,7 +56,7 @@ model = keras.models.load_model(model_path)
 def prediction(im):
   im = np.asarray(im)
   im = np.expand_dims(im, axis=0)
-  st.write(model.predict(im))
+  return model.predict(im)
   
 
 st.set_page_config(layout = "wide")
