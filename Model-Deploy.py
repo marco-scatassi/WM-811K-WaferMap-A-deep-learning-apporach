72f67ad86f7fa93ffeaf6a68155db6e752caa023
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-08-02 11:28:06 +0200

    Update Model-Deploy.py

--- Model-Deploy.py
+++ Model-Deploy.py
@@ -85,8 +85,6 @@ if __name__ == '__main__':
     pill_im = imagesDeploy[failureTypes[0]][0]
     st.write(pill_im)
     im = np.asarray(pill_im)
-    st.write(im)
-    im = im[:,:,0:3]
     st.write(im.shape)
     im = np.expand_dims(im, axis=0)
     model.evaluate(im)
