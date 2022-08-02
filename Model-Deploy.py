a52830ceb73eef7d9dfd6382db3424de1cf04a30
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-08-02 11:14:20 +0200

    Update Model-Deploy.py

--- Model-Deploy.py
+++ Model-Deploy.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     pill_im = imagesDeploy[failureTypes[0]][0]
     st.write(pill_im)
     im = np.asarray(pill_im)
-    st.write(im)
+    st.write(im.shape)
     im.reshape((im.shape[0], im.shape[1], 4))
-    st.write(im.size)
+    st.write(im.shape)
     model.predict(im)
