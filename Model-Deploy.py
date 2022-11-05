ede926a71e5c5639f467877ccd294a57e1b445ec
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-11-05 19:36:30 +0100

    Update Model-Deploy.py

--- Model-Deploy.py
+++ Model-Deploy.py
@@ -83,10 +83,10 @@ if __name__ == '__main__':
   for i in range(len(cols)):
     with cols[i]:
       st.image(imagesDisplay[selectedType][i])
-      buttons.append(st.button('Image '+str(i), on_click = prediction, args = (imagesDeploy[selectedType][i],)))
+      buttons.append(st.button('Image '+str(i))#, on_click = prediction, args = (imagesDeploy[selectedType][i],)))
       st.write('##')
       st.image(imagesDisplay[selectedType][i+5])
-      buttons.append(st.button('Image '+str(i+5), on_click = prediction, args = (imagesDeploy[selectedType][i+5],)))
+      buttons.append(st.button('Image '+str(i+5))#, on_click = prediction, args = (imagesDeploy[selectedType][i+5],)))
       
   if(np.sum(buttons) > 0): 
     st.write(pr)
